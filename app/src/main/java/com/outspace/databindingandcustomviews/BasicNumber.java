51e1a546eac760ef4db3c7ab6eae31498476acc9
DigitalOnion <luis.viruena@gmail.com>
2017-08-07 22:57:58 -0700

    Add Data Binding and an Error pops up at compile time

---
+++ app/src/main/java/com/outspace/databindingandcustomviews/BasicNumber.java
@@ -0,0 +1,13 @@
+package com.outspace.databindingandcustomviews;
+
+public class BasicNumber {
+    private float theNumber = 0.0f;
+
+    public float getTheNumber() {
+        return theNumber;
+    }
+
+    public void setTheNumber(float theNumber) {
+        this.theNumber = theNumber;
+    }
+}
--- app/src/main/java/com/outspace/databindingandcustomviews/MainActivity.java
+++ app/src/main/java/com/outspace/databindingandcustomviews/MainActivity.java
@@ -1,13 +1,22 @@
 package com.outspace.databindingandcustomviews;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.outspace.databindingandcustomviews.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        BasicNumber number = new BasicNumber();
+        number.setTheNumber(3.14159264f);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setBasicNumber(number);
         setContentView(R.layout.activity_main);
     }
 }
